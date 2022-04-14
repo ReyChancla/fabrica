bcbd2aa89e93e247e6f430b9d8a8cc327e54fca3
ReyChancla <61477102+ReyChancla@users.noreply.github.com>
2022-04-15 00:00:05 +0200

    3

--- SrcriptCuri.js
+++ SrcriptCuri.js
@@ -1,15 +1,73 @@
 
+const DatosCanalesCurioni = [
+  {
+    nombre: "canalE",
+    valoresDiferenciaMartin: {
+      c1: 2,
+      c2: 4,
+      s: 30,
+    },
+    resta: 2,
+  }, 
+  {
+    nombre: "canalB",
+    valoresDiferenciaMartin: {
+      c1: 3,
+      c2: 5,
+      s: 30,
+    },
+    resta: 3,
+  },
+  {
+    nombre: "canalC",
+    valoresDiferenciaMartin: {
+      c1: 4,
+      c2: 7,
+      s: 30,
+    },
+    resta: 4,
+  },
+  {
+    nombre: "canalEB",
+    valoresDiferenciaMartin: {
+      c1: 5,
+      c2: 10,
+      s: 40,
+    },
+    resta: 4,
+  },
+  {
+    nombre: "canalBC",
+    valoresDiferenciaMartin: {
+      c1: 7,
+      c2: 14,
+      s: 40,
+    },
+    resta: 5,
+  },
+];
+
+function datosCurioniHendidos() {
+  const CanalCurioni = document.getElementById("CanalCurioni").value;
+  const SelectGrade = DatosCanalesCurioni.find((grade) =>{
+    return grade.nombre ===CanalCurioni
+  });
+  const {nombre, valoresDiferenciaMartin:{ c1, c2, s},resta} = SelectGrade;
+  return {c1, c2, s, resta};
+}
+
 function DatosCurioni() {
   //medidas plancha
   const ancho = Number(document.getElementById("anchoPlanchaCuri").value);
   const largo = Number(document.getElementById("largoPlanchaCuri").value);
 //Medidas interiores
-  const Ancho = Number(document.getElementById("largoPlanchaCuri").value);
-  const Largo = Number(document.getElementById("largoPlanchaCuri").value);
-  const Alto = Number(document.getElementById("largoPlanchaCuri").value);
+  const Largo = Number(document.getElementById("largoCuri").value);
+  const Ancho = Number(document.getElementById("anchoCuri").value);
+  const Alto = Number(document.getElementById("altoCuri").value);
 
   const NTintas = Number(document.getElementById("numerodetintas").value);
   const Coste = Number(document.getElementById("TipoDeIMpresion").value);
+  const canal = document.getElementById("CanalCurioni").value;
   const Premontaje = 0.0022;
   return{
     ancho,
@@ -19,7 +77,8 @@ function DatosCurioni() {
     Alto,
     NTintas,
     Coste, 
-    Premontaje
+    Premontaje,
+    canal
   };
 }
 
@@ -42,32 +101,34 @@ function planchaImpresa() {
   const CosteTotal = (ancho * largo) * Coste ;
 
   console.log(NTintas);
-  console.log(Coste);
+  console.log(NTintas);
 
 
-  document.getElementById("optionDatos").innerHTML = '<H2>Datos para Docupoint:</H2>' + '<br>' +
+  document.getElementById("optionDatos").innerHTML = 
+  '<H2>Datos para Docupoint, plancha impresa:</H2>' + '<br>' +
   "Fecha: " + formatofecha + '<br>' +
   "Medidas de Plancha: " + (ancho) + " x " + (largo) + '<br>' +
   "Ruta:  Ond/ Curioni (UP22)" + '<br>' +
-  "Nº de Tintas: " + NTintas + '<br>' +
+  "Nº de tintas: " + NTintas + '<br>' +
   "Coste de cliche: " + ((Premontaje + (CosteTotal*NTintas))/100).toFixed(2) + " €" + '<br>' +
   '<span id="error1"></span>'+
   '<span id="error2"></span>'+
   '<span id="error3"></span>'+
   '<span id="error4"></span>'+
-  '<span id="error5"></span>'
+  '<span id="error5"></span>'+
+  '<span id="error6"></span>'
   ;
   
   //////////////////
 if (ancho < 350) {
-  document.getElementById("error1").innerHTML = '<strong>' + "El ancho tiene un mínimo y un máximo de 350 y 1300 mm." + '</strong><br>'
+  document.getElementById("error1").innerHTML =  "El ancho tiene un mínimo y un máximo de 350 y 1300 mm." + '<br>'
 }
 
 if (ancho > 1300) {
-  document.getElementById("error1").innerHTML = '<strong>' + "El ancho tiene un mínimo y un máximo de 350 y 1300 mm." + '</strong><br>'
+  document.getElementById("error1").innerHTML =  "El ancho tiene un mínimo y un máximo de 350 y 1300 mm." + '<br>'
 }
 if (largo > 2900) {
-  document.getElementById("error2").innerHTML = '<strong>' + "El largo tiene un máximo de 2900 mm." + '</strong><br>'
+  document.getElementById("error2").innerHTML =  + "El largo tiene un máximo de 2900 mm." + '<br>'
 }
 if (NTintas < 1) {
   document.getElementById("error3").innerHTML = "Si no lleva impresión sale directa de Onduladora" + '<br>';
@@ -80,6 +141,8 @@ if (ancho > 1300) {
 if (ancho > 1250) {
   document.getElementById("error5").innerHTML = "Impresión máxima de cliché  1250 mm y una pantalla no superior a 1300 mm. " + "Reimprime a partir de 1335 mm" + '<br>';
 };
+
+
 //////////////////
 
 
@@ -87,38 +150,274 @@ if (ancho > 1250) {
 
 
 function b1Pegada1() {
-  document.getElementById("optionBox").innerHTML = '<H2>Datos de cualculo:</H2>' + '<br>'+
-  '<input type="number" id="anchoPlanchaCuri" placeholder="Ancho de plancha"><b> x </b>'+
-  '<input type="number" id="largoPlanchaCuri" placeholder="Largo de plancha">'
+  const{Ancho, Largo, Alto, NTintas, Coste, Premontaje, canal} = DatosCurioni();
+  const {formatofecha} = fecha();
+  const {c1,c2,s, resta} = datosCurioniHendidos();
+////////////////////////////
+
+console.log(canal);
+
+////////////////////////////
+
+  //Calculos de medidas//
+const AnchoT = Ancho + c1;
+const LargoT = Largo + c1;
+const AltoT = Alto + c2;
+const pestaña = s;
+const solapa = Math.trunc(Ancho/2) + resta;
+const anchoPlanchaCuri = AltoT + (solapa * 2);
+const largoPlanchaCuri = (LargoT * 2) + (AnchoT * 2) + s;
+  
+
+
+  //////////////////////////////////////
+  //////////////////////////////////////
+
+  document.getElementById("optionDatos").innerHTML =
+  '<h2>Datos para Curioni, B1 pegada en el ancho</h2><br>' + 
+  "Fecha: " + formatofecha + '<br>' + 
+  "Mididas de plancha: " + anchoPlanchaCuri + " x " + largoPlanchaCuri + '<br>' + 
+  "Ruta: " + "Onduladora/ Curioni(UP22)" + '<span id="dobleRuta1"></span>' + '<span id="dobleRuta2"></span>' + '<br>' + 
+  "Nº de tintas: " + NTintas + '<br>' + 
+  "Coste de cliches: " + ((((anchoPlanchaCuri * largoPlanchaCuri)*NTintas)/100) * (Coste + Premontaje)).toFixed(2) + '<br>' + 
+  '<span id="error1"></span>'+
+  '<span id="error2"></span>'+
+  '<span id="error3"></span>'+
+  '<span id="error4"></span>'+
+  '<span id="error5"></span>'+
+  '<span id="error6"></span>'+
+  '<span id="error7"></span>'+
+  '<span id="error8"></span>'+
+  '<span id="error9"></span>'
+
+
+  //////////////////////////////////////
+  //////////////////////////////////////
+  if (AnchoT <= 205) {
+    document.getElementById("error1").innerHTML = " Esta B1 devería de ser fabricada con la pestaña en el ancho. " + '<br>';
+    document.getElementById("dobleRuta1").innerHTML = " / Curioni(UP21)";
+  }
+  if (AnchoT <= 250) {
+    document.getElementById("error2").innerHTML = " Si va troquelada en el ancho con agujeros, ha de tener dos pasadas" + '<br>';
+    document.getElementById("dobleRuta1").innerHTML = " / Curioni(UP21) ";
+  }
+  if (AnchoT <= 300) {
+    document.getElementById("error3").innerHTML = " Si va troquelada en el ancho con asa, ha de tener dos pasadas" + '<br>';
+    document.getElementById("dobleRuta2").innerHTML = " / Curioni(UP21) ";
+  }
+  if (LargoT > 1050) {
+    document.getElementById("error4").innerHTML = "Fabricar con maquina abierta. " + '<br>';
+    document.getElementById("dobleRuta2").innerHTML =  "/ Pegadora manual ";
+  }
+  if (anchoPlanchaCuri > 310 && anchoPlanchaCuri < 350) {
+    document.getElementById("error5").innerHTML = "Esta caja solo puede ser anonima, sin impresión" + '<br>';
+  }
+  if (anchoPlanchaCuri > 1250) {
+    document.getElementById("error6").innerHTML = "Desmontar tacos." + '<br>';
+  }
+  if (anchoPlanchaCuri > 1300) {
+    document.getElementById("error6").innerHTML = "Desmontar tacos y  usar Skip Feed." + '<br>';
+  }
+  if ((AltoT + solapa) > 1300) {
+    document.getElementById("error7").innerHTML = "Compensador de altura, No se pueden poner rendijas y asas troqueladas a la vez (dos pasadas)."
+    document.getElementById("dobleRuta1").innerHTML = " / Curioni(UP21) ";
+  }
+  if ((AltoT + solapa) > 2000) {
+    document.getElementById("error7").innerHTML = "Sobre pasa el ancho de plancha maximo." + '<br>';
+  }
+  if (canal === "canalE") {
+    document.getElementById("error8").innerHTML = "El canal E,no se puede usar para este modelo";
+    alert("Has usado un canal, que no puede ser usado para este modelo")
+  }
+
+  //////////////////////////////////////
+  //////////////////////////////////////
+
 }
 
 function b1Pegada2() {
-  document.getElementById("optionBox").innerHTML = '<H2>Datos de cualculo:</H2>' + '<br>'+
-  '<input type="number" id="largoCuri" placeholder="largo"><b> x </b>'+
-  '<input type="number" id="anchoCuri" placeholder="ancho"><b> x </b>'+
-  '<input type="number" id="altoCuri" placeholder="alto">';
+  const{Ancho, Largo, Alto, NTintas, Coste, Premontaje, canal} = DatosCurioni();
+  const {formatofecha} = fecha();
+  const {c1,c2,s, resta} = datosCurioniHendidos();
+////////////////////////////
+
+console.log(canal);
+
+////////////////////////////
+
+  //Calculos de medidas//
+const AnchoT = Ancho + c1;
+const LargoT = Largo + c1;
+const AltoT = Alto + c2;
+const pestaña = s;
+const solapa = Math.trunc(Ancho/2) + resta;
+const anchoPlanchaCuri = AltoT + (solapa * 2);
+const largoPlanchaCuri = (LargoT * 2) + (AnchoT * 2) + s;
+  
+
+
+  //////////////////////////////////////
+  //////////////////////////////////////
+
+  document.getElementById("optionDatos").innerHTML =
+  '<h2>Datos para Curioni, B1 pegada en el largo</h2><br>' + 
+  "Fecha: " + formatofecha + '<br>' + 
+  "Mididas de plancha: " + anchoPlanchaCuri + " x " + largoPlanchaCuri + '<br>' + 
+  "Ruta: " + "Onduladora/ Curioni(UP22)" + '<span id="dobleRuta1"></span>' +
+  '<span id="dobleRuta2"></span>' + '<span id="dobleRuta3"></span>' + '<br>' + 
+  "Nº de tintas: " + NTintas + '<br>' + 
+  "Coste de cliches: " + ((((anchoPlanchaCuri * largoPlanchaCuri)*NTintas)/100) * (Coste + Premontaje)).toFixed(2) +'<br>' + 
+  '<span id="error1"></span>'+
+  '<span id="error2"></span>'+
+  '<span id="error3"></span>'+
+  '<span id="error4"></span>'+
+  '<span id="error5"></span>'+
+  '<span id="error6"></span>'+
+  '<span id="error7"></span>'+
+  '<span id="error8"></span>'+
+  '<span id="error9"></span>'+
+  '<span id="error10"></span>'+
+  '<span id="error11"></span>'
+
+
+  //////////////////////////////////////
+  //////////////////////////////////////
+  if (LargoT > 1000) {
+    document.getElementById("error1").innerHTML = " Fabricar con maquina abierta y pegar en pegadora manual" + '<br>';
+    document.getElementById("dobleRuta2").innerHTML = " / Pegadora manual ";
+  }
+if (AnchoT < 70) {
+    document.getElementById("error2").innerHTML = " Usar maquina abierta, dar doble pasada y pegado manual. " + '<br>';
+    document.getElementById("dobleRuta1").innerHTML = " / Curioni(UP21)";
+    document.getElementById("dobleRuta2").innerHTML =  "/ Pegadora manual ";
+  }
+  if (70 < AnchoT && 140 > AltoT) {
+    document.getElementById("error3").innerHTML = " Ha de fabricarse con la maquina abierta y con pegado manual. Falsos endidos en caras largas" + '<br>';
+    document.getElementById("dobleRuta2").innerHTML = " / Pegadora manual ";
+  }
+  if (AnchoT <= 250) {
+    document.getElementById("error4").innerHTML = " Si va troquelada en el ancho con agujeros, ha de tener dos pasadas" + '<br>';
+    document.getElementById("dobleRuta1").innerHTML = " / Curioni(UP21) ";
+  }
+  if (AnchoT <= 300) {
+    document.getElementById("error5").innerHTML = " Si va troquelada en el ancho con asa, ha de tener dos pasadas" + '<br>';
+    document.getElementById("dobleRuta1").innerHTML = " / Curioni(UP21) ";
+  }
+  if (anchoPlanchaCuri > 310 && anchoPlanchaCuri < 350) { 
+    document.getElementById("error6").innerHTML = "Esta caja solo puede ser anonima, sin impresión" + '<br>';
+  }
+  if (anchoPlanchaCuri > 1250) {
+    document.getElementById("error7").innerHTML = "Desmontar tacos." + '<br>';
+  }
+  if (anchoPlanchaCuri > 1300) {
+    document.getElementById("error7").innerHTML = "Desmontar tacos y  usar Skip Feed." + '<br>';
+  }
+  if ((AltoT + solapa) > 1300) {
+    document.getElementById("error8").innerHTML = "Compensador de altura, No se pueden poner rendijas y asas troqueladas a la vez (dos pasadas)."
+    document.getElementById("dobleRuta1").innerHTML = " / Curioni(UP21) ";
+  }
+  if ((AltoT + solapa) > 2000) {
+    document.getElementById("error8").innerHTML = "Sobre pasa el ancho de plancha maximo." + '<br>';
+  }
+  if (largoPlanchaCuri > 3000) {
+    document.getElementById("error10").innerHTML = "Esta caja sobre pasa el maximo de ancho de plancha";
+  }
+  if (largoPlanchaCuri > 2900) {
+    document.getElementById("error10").innerHTML = "No puede sobrepasar los 2900 mm de impresión.";
+  }
+if (canal === "canalE") {
+    document.getElementById("error11").innerHTML = "El canal E,no se puede usar para este modelo";
+    alert("El canal E no se puede usar para este modelo");
+  }
+
+  //////////////////////////////////////
+  //////////////////////////////////////
 }
 
 function mediaCaja() {
-  document.getElementById("optionBox").innerHTML = '<H2>Datos de cualculo:</H2>' + '<br>'+
-  '<input type="number" id="largoCuri" placeholder="largo"><b> x </b>'+
-  '<input type="number" id="anchoCuri" placeholder="ancho"><b> x </b>'+
-  '<input type="number" id="altoCuri" placeholder="alto">';
+  const{Ancho, Largo, Alto, NTintas, Coste, Premontaje, canal} = DatosCurioni();
+  const {formatofecha} = fecha();
+  const {c1,c2,s, resta} = datosCurioniHendidos();
+////////////////////////////
+
+/* console.log(canal); */
+
+////////////////////////////
+
+  //Calculos de medidas//
+const AnchoT = Ancho + c1;
+const LargoT = Largo + c1;
+const AltoT = Alto + c2;
+const pestaña = s;
+const solapa = Math.trunc(Ancho/2) + resta;
+const anchoPlanchaCuri = AltoT + (solapa * 2);
+const largoPlanchaCuri = (LargoT + AnchoT) + s;
+  
+
+
+  //////////////////////////////////////
+  //////////////////////////////////////
+
+  document.getElementById("optionDatos").innerHTML =
+  '<h2>Datos para Curioni, Media Caja</h2><br>' + 
+  "Fecha: " + formatofecha + '<br>' + 
+  "Mididas de plancha: " + anchoPlanchaCuri + " x " + largoPlanchaCuri + '<br>' + 
+  "Ruta: " + "Onduladora/ Curioni(UP22)" + '<span id="dobleRuta1"></span>' +
+  '<span id="dobleRuta2"></span>' + '<span id="dobleRuta3"></span>' + '<br>' + 
+  "Nº de tintas: " + NTintas + '<br>' + 
+  "Coste de cliches: " + ((((anchoPlanchaCuri * largoPlanchaCuri)*NTintas)/100) * (Coste + Premontaje)).toFixed(2) +'<br>' + 
+  '<span id="error1"></span>'+
+  '<span id="error2"></span>'+
+  '<span id="error3"></span>'+
+  '<span id="error4"></span>'+
+  '<span id="error5"></span>'+
+  '<span id="error6"></span>'+
+  '<span id="error7"></span>'+
+  '<span id="error8"></span>'+
+  '<span id="error9"></span>'+
+  '<span id="error10"></span>'+
+  '<span id="error11"></span>'
+
+
+  //////////////////////////////////////
+  //////////////////////////////////////
+  if (AnchoT < 205) {
+    document.getElementById("error1").innerHTML = " Dar doble pasada. " + '<br>';
+    document.getElementById("dobleRuta1").innerHTML = " / Curioni(UP21)";
+  }
+  if (AnchoT < 250) {
+    document.getElementById("error2").innerHTML = " Si tiene agujeros en el ancho, dar dos pasadas " + '<br>';
+    document.getElementById("dobleRuta1").innerHTML = " / Curioni(UP21)";
+  }
+  if (AnchoT < 300) {
+    document.getElementById("error3").innerHTML = " Si tiene asas en el ancho, dar dos pasadas " + '<br>';
+    document.getElementById("dobleRuta1").innerHTML = " / Curioni(UP21)";
+  }
+  if (310 < anchoPlanchaCuri < 350) {
+    document.getElementById("error4").innerHTML = " Esta caja ha de ser anonima, sin impresión" + '<br>';
+  }
+  if (anchoPlanchaCuri > 1300) {
+    document.getElementById("error5").innerHTML = " Skip Feed" + '<br>';
+  }
+  if ((AltoT + solapa) >1300) {
+    document.getElementById("error5").innerHTML = " Skip Feed" + '<br>' + 
+    " Usar compensador de altura" + '<br>' +
+    " No se puede hacer rendijas y asas a la vez. Requiere dos pasadas" + '<br>';
+    document.getElementById("dobleRuta1").innerHTML = " / Curioni(UP21)";
+  }
+  if (anchoPlanchaCuri > 2000) {
+    document.getElementById("error5").innerHTML = " La plancha supera el maximo del ancho " + '<br>';
+  }
+  if (canal === "canalE") {
+   alert("El canal E, no se puede usar en este modelo de cajas");
+  }
+
 }
 
 function cajaTelescopica() {
-  const ancho = Number(document.getElementById("anchoPlanchaCuri").value);
-  const largo = Number(document.getElementById("largoPlanchaCuri").value);
-
-  console.log(largo);
-  console.log(ancho);
-
-  document.getElementById("optionDatos").innerHTML = '<H2>Datos para Docupoint:</H2>' + '<br>' +
-  "adios"
+document.getElementById("optionDatos").innerHTML = '<div id=error1>Hay que esperar a que termine esta parte</div>'
 }
 
 function Fefco409() {
-  document.getElementById("optionBox").innerHTML = '<H2>Datos de cualculo:</H2>' + '<br>'+
-  '<input type="number" id="anchoPlanchaCuri" placeholder="Ancho de plancha"><b> x </b>'+
-  '<input type="number" id="largoPlanchaCuri" placeholder="Largo de plancha">'
+  document.getElementById("optionDatos").innerHTML = '<div id=error1>Hay que esperar a que termine esta parte</div>'
 }
